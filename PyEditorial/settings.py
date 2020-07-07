6df4bda2001bf7958446cb2dd20f710aa178701e
mavenium <mavenium@gmail.com>
2020-07-07 17:01:22 +0430

    add core app

--- PyEditorial/settings.py
+++ PyEditorial/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'PyEditorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
