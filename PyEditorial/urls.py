05d31c403cee4dd73e6b4965de19a2f4468906b0
mavenium <mavenium@gmail.com>
2020-07-08 12:26:44 +0430

    add "STATIC_URL" and "MEDIA_URL" to urls.py

--- PyEditorial/urls.py
+++ PyEditorial/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
