122320959ed2703f290068692641fb9ced435165
mavenium <mavenium@gmail.com>
2020-07-08 15:07:10 +0430

    send config to index.html

--- PyEditorial/views.py
+++ PyEditorial/views.py
@@ -1,11 +1,14 @@
 from django.views import generic
 
+from constance import config
+
 
 class Index(generic.ListView):
     template_name = 'index.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['config'] = config
         return context
 
     def get_queryset(self):
