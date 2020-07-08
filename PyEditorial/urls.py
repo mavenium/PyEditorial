bd51363df1b5e5ed5faf84b1db49df05fb5b51c7
mavenium <mavenium@gmail.com>
2020-07-08 12:58:29 +0430

    add "index" to urlpatterns

--- PyEditorial/urls.py
+++ PyEditorial/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Index.as_view(), name="index"),
 ]
 
 if settings.DEBUG:
