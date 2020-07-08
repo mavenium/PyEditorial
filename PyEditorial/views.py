e644bbbbd60ed73c47e2a602a6227b2a28051637
mavenium <mavenium@gmail.com>
2020-07-08 20:25:26 +0430

    fix constance config

--- PyEditorial/views.py
+++ PyEditorial/views.py
@@ -1,8 +1,15 @@
+from django.dispatch import receiver
 from django.views import generic
 
-from constance import config
-
 from blog import models as BlogModels
+
+from constance import config
+from constance.signals import config_updated
+
+
+@receiver(config_updated)
+def constance_updated(sender, key, old_value, new_value, **kwargs):
+    print(sender, key, old_value, new_value)
 
 
 class Index(generic.ListView):
