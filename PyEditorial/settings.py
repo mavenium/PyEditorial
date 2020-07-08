89973f8afa2b43dc21bf95857c07aa15fae5d90f
mavenium <mavenium@gmail.com>
2020-07-08 12:17:57 +0430

    add "Static" and "Media" Settings

--- PyEditorial/settings.py
+++ PyEditorial/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'PyEditorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "templates/static"),
+]
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # CONSTANCE Settings
 CONSTANCE_BACKEND = 'constance.backends.memory.MemoryBackend'
