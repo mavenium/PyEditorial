b85548472746d9ba79f8d0b751c8664f3e16685d
mavenium <mavenium@gmail.com>
2020-07-08 12:58:55 +0430

    add "Index" Class

---
+++ PyEditorial/views.py
@@ -0,0 +1,12 @@
+from django.views import generic
+
+
+class Index(generic.ListView):
+    template_name = 'index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        pass
