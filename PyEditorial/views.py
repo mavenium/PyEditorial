13d05cc127b130c8584f7038cd3e4a21fe87479a
mavenium <mavenium@gmail.com>
2020-07-08 18:26:07 +0430

    show last blog post in index

--- PyEditorial/views.py
+++ PyEditorial/views.py
@@ -2,12 +2,15 @@ from django.views import generic
 
 from constance import config
 
+from blog import models as BlogModels
+
 
 class Index(generic.ListView):
     template_name = 'index.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['last_blog_post'] = BlogModels.Post.objects.order_by('-pk')[:1]
         context['config'] = config
         return context
 
