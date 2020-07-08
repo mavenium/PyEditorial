4bd61ad325bf686a174c306f78ea44f1c5d2b55b
mavenium <mavenium@gmail.com>
2020-07-08 16:07:45 +0430

    create Category and Post class model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=256, verbose_name="Title :")
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256, verbose_name="Title :")
+    slug = models.SlugField(max_length=256, verbose_name="Slug :")
+    thumbnail = models.ImageField(upload_to='blog/thumbnail/', verbose_name="Thumbnail :")
+    category = models.ManyToManyField(Category)
+
+    def display_category(self):
+        return ', '.join([cat.title for cat in self.category.all()])
+
+    def __str__(self):
+        return self.title
+
+
+
