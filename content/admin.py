703aa5aeccf6b07d03d7e5c737c7b2b5ebfcf24d
mavenium <mavenium@gmail.com>
2020-07-10 17:49:35 +0430

    fix admins classes

--- content/admin.py
+++ content/admin.py
@@ -5,19 +5,28 @@ from . import models
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'display_category']
+    list_display = ['title', 'display_category', 'publish']
+    list_editable = ('publish',)
+    list_filter = ('publish', 'category',)
+    search_fields = ('title',)
     models.Blog.display_category.short_description = 'Categories'
     prepopulated_fields = {'slug': ('title',)}
 
 
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ['title', 'display_category']
+    list_display = ['title', 'display_category', 'publish']
+    list_editable = ('publish',)
+    list_filter = ('publish', 'category',)
+    search_fields = ('title',)
     models.Videocast.display_category.short_description = 'Categories'
     prepopulated_fields = {'slug': ('title',)}
 
 
 class PodcastAdmin(admin.ModelAdmin):
-    list_display = ['title', 'display_category']
+    list_display = ['title', 'display_category', 'publish']
+    list_editable = ('publish',)
+    list_filter = ('publish', 'category',)
+    search_fields = ('title',)
     models.Podcast.display_category.short_description = 'Categories'
     prepopulated_fields = {'slug': ('title',)}
 
