a8290347b131fffd6b5b8276ada88d9c07ac00f4
mavenium <mavenium@gmail.com>
2020-07-10 11:38:45 +0430

    add Blog, Videocast,Podcast, Skills models

--- PyEditorial/views.py
+++ PyEditorial/views.py
@@ -10,8 +10,14 @@ class Index(generic.ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['last_blog_post'] = ContentModels.Blog.objects.order_by('-pk').filter(publish=True)[:1]
-        context['blog_posts'] = ContentModels.Blog.objects.order_by('-pk').filter(publish=True)[1:5]
+        context['last_blog'] = ContentModels.Blog.objects.order_by('-pk').filter(publish=True)[:1]
+        context['skills'] = ContentModels.Skill.objects.all()
+        context['blogs'] = ContentModels.Blog.objects.order_by('-pk').filter(publish=True)[1:5]
+        context['blogs_categories'] = ContentModels.BlogCategory.objects.all()
+        context['videocasts'] = ContentModels.Videocast.objects.order_by('-pk').filter(publish=True)[1:5]
+        context['videocasts_categories'] = ContentModels.VideocastCategory.objects.all()
+        context['podcast'] = ContentModels.Podcast.objects.order_by('-pk').filter(publish=True)[1:5]
+        context['podcast_categories'] = ContentModels.PodcastCategory.objects.all()
         context['config'] = config
         return context
 
