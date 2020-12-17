4ae98a272c0f9cf1d4d3da3f9b069912dffc7fad
Mahdi Namaki <m.namaki@qbicomm.com>
2020-12-17 14:59:31 +0330

    fix admin class name and use translation

--- content/admin.py
+++ content/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from . import models
 
 
-# Register your models here.
-class PostAdmin(admin.ModelAdmin):
+class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'display_category', 'publish']
     list_editable = ('publish',)
     list_filter = ('publish', 'category',)
     search_fields = ('title',)
-    models.Blog.display_category.short_description = 'Categories'
+    models.Blog.display_category.short_description = _('Categories')
     prepopulated_fields = {'slug': ('title',)}
 
 
@@ -18,7 +18,7 @@ class VideoAdmin(admin.ModelAdmin):
     list_editable = ('publish',)
     list_filter = ('publish', 'category',)
     search_fields = ('title',)
-    models.Videocast.display_category.short_description = 'Categories'
+    models.VideoCast.display_category.short_description = _('Categories')
     prepopulated_fields = {'slug': ('title',)}
 
 
@@ -27,20 +27,14 @@ class PodcastAdmin(admin.ModelAdmin):
     list_editable = ('publish',)
     list_filter = ('publish', 'category',)
     search_fields = ('title',)
-    models.Podcast.display_category.short_description = 'Categories'
+    models.Podcast.display_category.short_description = _('Categories')
     prepopulated_fields = {'slug': ('title',)}
 
 
-admin.site.register(models.Blog, PostAdmin)
-
 admin.site.register(models.BlogCategory)
-
-admin.site.register(models.Videocast, VideoAdmin)
-
-admin.site.register(models.VideocastCategory)
-
+admin.site.register(models.Blog, BlogAdmin)
+admin.site.register(models.VideoCastCategory)
+admin.site.register(models.VideoCast, VideoAdmin)
 admin.site.register(models.Podcast, PodcastAdmin)
-
 admin.site.register(models.PodcastCategory)
-
 admin.site.register(models.Skill)
