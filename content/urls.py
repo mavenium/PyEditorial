324fb36cc4661120bfe3875db0ef98e6ee2e8efb
Mahdi Namaki <mavenium@gmail.com>
2020-12-29 10:18:43 +0330

    add create blog category url

--- content/urls.py
+++ content/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('search/', views.Search.as_view(), name='search'),
     path('blog/', views.Blog.as_view(), name='blog'),
     path('create/blog/', views.BlogCreateView.as_view(), name='blog_create'),
+    path('create/blog_category/', views.BlogCategoryCreateView.as_view(), name='blog_category_create'),
     path('blog/<int:pk>/', views.BlogArchiveByCategoryPK.as_view(), name='blog_archive_by_category_pk'),
     path('blog/<str:slug>/', views.BlogSingle.as_view(), name='blog_single'),
     path('video_cast/', views.VideoCast.as_view(), name='video_cast'),
