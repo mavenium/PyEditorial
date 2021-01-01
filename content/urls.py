f94780bcf5080268809ad281eb3c843e88de29a8
Mahdi Namaki <mavenium@gmail.com>
2021-01-01 12:11:34 +0330

    add video_cast_create url

--- content/urls.py
+++ content/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('blog/<str:slug>/', views.BlogSingle.as_view(), name='blog_single'),
     path('create/video_cast_category/', views.VideoCastCategoryCreateView.as_view(), name='video_cast_category_create'),
     path('video_cast/', views.VideoCast.as_view(), name='video_cast'),
+    path('create/video_cast/', views.VideoCastCreateView.as_view(), name='video_cast_create'),
     path('video_cast/<int:pk>/', views.VideoCastArchiveByCategoryPK.as_view(), name='video_cast_archive_by_category_pk'),
     path('video_cast/<str:slug>/', views.VideoCastSingle.as_view(), name='video_cast_single'),
     path('create/podcast_category/', views.PodcastCategoryCreateView.as_view(), name='podcast_category_create'),
