1b46aeb8fc3478ca49a41aec6fb40f99030faa2f
Mahdi Namaki <mavenium@gmail.com>
2021-01-03 11:18:21 +0330

    create skill_create url

--- content/urls.py
+++ content/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('create/podcast/', views.PodcastCreateView.as_view(), name='podcast_create'),
     path('podcast/<int:pk>/', views.PodArchiveByCategoryPK.as_view(), name='podcast_archive_by_category_pk'),
     path('podcast/<str:slug>/', views.PodSingle.as_view(), name='podcast_single'),
+    path('create/skill/', views.SkillCreateView.as_view(), name='skill_create'),
 ]
