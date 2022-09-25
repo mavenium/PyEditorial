13fa037959bb15f68ff5a722323a4580aed430a6
Bhavya Seth <bhavyaseth.23@gmail.com>
2022-09-25 23:11:17 +0530

    database change

--- PyEditorial/settings.py
+++ PyEditorial/settings.py
@@ -80,11 +80,30 @@ WSGI_APPLICATION = 'PyEditorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'PyEditorial',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'great123',
+
+        'HOST': 'localhost',
+
+        'PORT': '',
+
     }
+
 }
 
 
